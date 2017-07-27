0812212a84ff5868d9384f997cf24dd566f94bea
Dan Cutting <dan@cutting.io>
2017-07-27 14:48:08 +0100

    Advert tests.

---
+++ RemixLogicTests/Entity/AdvertTests.swift
@@ -0,0 +1,39 @@
+//  Copyright © 2017 cutting.io. All rights reserved.
+
+import XCTest
+
+class AdvertTests: XCTestCase {
+
+    func test_equal_same_returnsTrue() {
+
+        let advertA = makeAdvert(advertID: "1", title: "hello world", categoryID: "9")
+        let advertB = makeAdvert(advertID: "1", title: "hello world", categoryID: "9")
+
+        XCTAssertEqual(advertA, advertB)
+    }
+
+    func test_equal_differentID_returnsFalse() {
+
+        let advertA = makeAdvert(advertID: "1", title: "hello world", categoryID: "9")
+        let advertB = makeAdvert(advertID: "2", title: "hello world", categoryID: "9")
+
+        XCTAssertNotEqual(advertA, advertB)
+    }
+
+    func test_equal_differentTitle_returnsFalse() {
+
+        let advertA = makeAdvert(advertID: "1", title: "hello world", categoryID: "9")
+        let advertB = makeAdvert(advertID: "1", title: "goodbye world", categoryID: "9")
+
+        XCTAssertNotEqual(advertA, advertB)
+    }
+
+    func test_equal_differentCategoryID_returnsFalse() {
+
+        let advertA = makeAdvert(advertID: "1", title: "hello world", categoryID: "9")
+        let advertB = makeAdvert(advertID: "1", title: "hello world", categoryID: "7")
+
+        XCTAssertNotEqual(advertA, advertB)
+    }
+}
+
--- RemixLogicTests/Entity/CategoryTests.swift
+++ RemixLogicTests/Entity/CategoryTests.swift
@@ -6,32 +6,40 @@ class CategoryTests: XCTestCase {
 
     func test_equal_same_returnsTrue() {
 
-        let categoryA = makeCategory(categoryID: "1", children: ["4", "6"], title: "hello world")
-        let categoryB = makeCategory(categoryID: "1", children: ["4", "6"], title: "hello world")
+        let categoryA = makeCategory(categoryID: "1", parent: "6", children: ["4", "6"], title: "hello world")
+        let categoryB = makeCategory(categoryID: "1", parent: "6", children: ["4", "6"], title: "hello world")
 
         XCTAssertEqual(categoryA, categoryB)
     }
 
     func test_equal_differentID_returnsFalse() {
 
-        let categoryA = makeCategory(categoryID: "1", children: ["4", "6"], title: "hello world")
-        let categoryB = makeCategory(categoryID: "2", children: ["4", "6"], title: "hello world")
+        let categoryA = makeCategory(categoryID: "1", parent: "6", children: ["4", "6"], title: "hello world")
+        let categoryB = makeCategory(categoryID: "2", parent: "6", children: ["4", "6"], title: "hello world")
+
+        XCTAssertNotEqual(categoryA, categoryB)
+    }
+
+    func test_equal_differentParent_returnsFalse() {
+
+        let categoryA = makeCategory(categoryID: "1", parent: "3", children: ["4", "6"], title: "hello world")
+        let categoryB = makeCategory(categoryID: "1", parent: "6", children: ["4", "6"], title: "hello world")
 
         XCTAssertNotEqual(categoryA, categoryB)
     }
 
     func test_equal_differentChildren_returnsFalse() {
 
-        let categoryA = makeCategory(categoryID: "1", children: ["4", "6"], title: "hello world")
-        let categoryB = makeCategory(categoryID: "1", children: ["4"], title: "hello world")
+        let categoryA = makeCategory(categoryID: "1", parent: "6", children: ["4", "6"], title: "hello world")
+        let categoryB = makeCategory(categoryID: "1", parent: "6", children: ["4"], title: "hello world")
 
         XCTAssertNotEqual(categoryA, categoryB)
     }
 
     func test_equal_differentTitle_returnsFalse() {
 
-        let categoryA = makeCategory(categoryID: "1", children: ["4", "6"], title: "hello world")
-        let categoryB = makeCategory(categoryID: "1", children: ["4", "6"], title: "goddbye world")
+        let categoryA = makeCategory(categoryID: "1", parent: "6", children: ["4", "6"], title: "hello world")
+        let categoryB = makeCategory(categoryID: "1", parent: "6", children: ["4", "6"], title: "goddbye world")
 
         XCTAssertNotEqual(categoryA, categoryB)
     }
