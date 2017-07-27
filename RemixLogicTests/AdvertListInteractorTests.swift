b293af37b1361dd5ff46a46afef56156ce0976c8
Dan Cutting <dan@cutting.io>
2017-07-27 13:41:15 +0100

    Test the advert list interactor filters adverts by category.

--- RemixLogicTests/AdvertListInteractorTests.swift
+++ RemixLogicTests/AdvertListInteractorTests.swift
@@ -32,8 +32,41 @@ class AdvertListInteractorTests: XCTestCase {
         mockCategoryService.categories = mockCategories
 
         subjectUnderTest.update(for: nil) { (actualAdverts, actualCategories) in
-            XCTAssertEqual(mockAdverts, actualAdverts)
-            XCTAssertEqual(mockCategories, actualCategories)
+
+            let expectedAdverts = mockAdverts
+            XCTAssertEqual(expectedAdverts, actualAdverts)
+
+            let expectedCategories = mockCategories
+            XCTAssertEqual(expectedCategories, actualCategories)
+        }
+    }
+
+    func test_update_setCategory_returnsAllCategoriesAndAdvertsInThatCategory() {
+
+        let mockAdverts = [
+            makeAdvert(advertID: "1", categoryID: "1"),
+            makeAdvert(advertID: "2", categoryID: "1"),
+            makeAdvert(advertID: "3", categoryID: "2"),
+            makeAdvert(advertID: "4", categoryID: "3")
+        ]
+        let mockCategories = [
+            makeCategory(categoryID: "1"),
+            makeCategory(categoryID: "2"),
+            makeCategory(categoryID: "3")
+        ]
+
+        mockAdvertService.adverts = mockAdverts
+        mockCategoryService.categories = mockCategories
+
+        subjectUnderTest.update(for: "1") { (actualAdverts, actualCategories) in
+
+            let expectedAdverts = [
+                mockAdverts[0], mockAdverts[1]
+            ]
+            XCTAssertEqual(expectedAdverts, actualAdverts)
+
+            let expectedCategories = mockCategories
+            XCTAssertEqual(expectedCategories, actualCategories)
         }
     }
 }
