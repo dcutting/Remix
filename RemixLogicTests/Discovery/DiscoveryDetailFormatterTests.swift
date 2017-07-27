d8cd84e3cc4fda363918700c73d424608d89bfc4
Dan Cutting <dan@cutting.io>
2017-07-27 13:58:08 +0100

    DiscoveryDetailFormatter tests.

---
+++ RemixLogicTests/Discovery/DiscoveryDetailFormatterTests.swift
@@ -0,0 +1,20 @@
+//  Copyright © 2017 cutting.io. All rights reserved.
+
+import XCTest
+
+class DiscoveryDetailFormatterTests: XCTestCase {
+
+    var subjectUnderTest: DiscoveryDetailFormatter!
+
+    override func setUp() {
+        subjectUnderTest = DiscoveryDetailFormatter()
+    }
+
+    func test_prepare_returnsTitle() {
+        let advert = makeAdvert(advertID: "1", title: "hello world")
+
+        let actual = subjectUnderTest.prepare(advert: advert)
+
+        XCTAssertEqual("hello world", actual.title)
+    }
+}
