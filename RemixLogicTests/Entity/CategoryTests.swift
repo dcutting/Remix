6fd0cedc982e8c764330e3d910ba775a531b9234
Dan Cutting <dan@cutting.io>
2017-07-27 14:27:08 +0100

    Category tests.

---
+++ RemixLogicTests/Entity/CategoryTests.swift
@@ -0,0 +1,38 @@
+//  Copyright © 2017 cutting.io. All rights reserved.
+
+import XCTest
+
+class CategoryTests: XCTestCase {
+
+    func test_equal_same_returnsTrue() {
+
+        let categoryA = makeCategory(categoryID: "1", children: ["4", "6"], title: "hello world")
+        let categoryB = makeCategory(categoryID: "1", children: ["4", "6"], title: "hello world")
+
+        XCTAssertEqual(categoryA, categoryB)
+    }
+
+    func test_equal_differentID_returnsFalse() {
+
+        let categoryA = makeCategory(categoryID: "1", children: ["4", "6"], title: "hello world")
+        let categoryB = makeCategory(categoryID: "2", children: ["4", "6"], title: "hello world")
+
+        XCTAssertNotEqual(categoryA, categoryB)
+    }
+
+    func test_equal_differentChildren_returnsFalse() {
+
+        let categoryA = makeCategory(categoryID: "1", children: ["4", "6"], title: "hello world")
+        let categoryB = makeCategory(categoryID: "1", children: ["4"], title: "hello world")
+
+        XCTAssertNotEqual(categoryA, categoryB)
+    }
+
+    func test_equal_differentTitle_returnsFalse() {
+
+        let categoryA = makeCategory(categoryID: "1", children: ["4", "6"], title: "hello world")
+        let categoryB = makeCategory(categoryID: "1", children: ["4", "6"], title: "goddbye world")
+
+        XCTAssertNotEqual(categoryA, categoryB)
+    }
+}
